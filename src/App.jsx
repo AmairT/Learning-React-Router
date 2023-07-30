29393660c303e2b1bdfe6c88a4e156349c9b699e
AmairT <amair0829@gmail.com>
2023-07-29 17:49:35 -0700

    color coded blue red home pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
 import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
@@ -5,15 +8,17 @@ function App() {
     return (
     <>
       <div id='container'>
-        <h1></h1>
+        <h1>Blue or Red!</h1>
         <div id='navbar'>
+          <Link to='/home'>Home</Link>
           <Link to='/blue'>Blue</Link>
           <Link to='/red'>Red</Link>
         </div>
         <div id='main-section'>
           <Routes>
-            <Route path='/blue' element={<h1>Blue</h1>} />
-            <Route path='/red' element={<h1>Red</h1>} />
+            <Route path='/blue' element={<Blue />} />
+            <Route path='/red' element={<Red />} />
+            <Route path='/home' element={<Home />} />
           </Routes>
         </div>
       </div> 
